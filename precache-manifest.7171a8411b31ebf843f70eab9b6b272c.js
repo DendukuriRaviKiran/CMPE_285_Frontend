e0dcdf189fffe132e1c2091e74be91c3224a421f
DendukuriRaviKiran <davidhume3690@gmail.com>
2023-05-21 18:27:28 -0700

    Updates

---
+++ precache-manifest.7171a8411b31ebf843f70eab9b6b272c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384ac4979bd1c0b01c18d3b81915a076",
+    "url": "http://18.191.67.212/index.html"
+  },
+  {
+    "revision": "62bae70a6be7f6187d58",
+    "url": "http://18.191.67.212/static/css/2.929389fa.chunk.css"
+  },
+  {
+    "revision": "475ed4de12406b4f5706",
+    "url": "http://18.191.67.212/static/css/main.7bedab26.chunk.css"
+  },
+  {
+    "revision": "62bae70a6be7f6187d58",
+    "url": "http://18.191.67.212/static/js/2.55010e4b.chunk.js"
+  },
+  {
+    "revision": "475ed4de12406b4f5706",
+    "url": "http://18.191.67.212/static/js/main.e9d49e09.chunk.js"
+  },
+  {
+    "revision": "cb37095d86c36701cb55",
+    "url": "http://18.191.67.212/static/js/runtime~main.3bcfc0c9.js"
+  }
+]);
