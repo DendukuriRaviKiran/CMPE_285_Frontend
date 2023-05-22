06c012e57220f861a1f4ea904b74bb3bdc54b108
DendukuriRaviKiran <davidhume3690@gmail.com>
2023-05-21 20:24:08 -0700

    Updates

--- precache-manifest.539472f31bd10963c1831fef49857725.js
+++ precache-manifest.539472f31bd10963c1831fef49857725.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d14f5b5a776e5151605ff4f331efb4fa",
+    "revision": "b08a08b06fa829a1eea319eaeacd0959",
     "url": "https://dendukuriravikiran.github.io/CMPE_285_frontend/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "https://dendukuriravikiran.github.io/CMPE_285_frontend/static/css/2.929389fa.chunk.css"
   },
   {
-    "revision": "bde8accedde27f17bbdf",
+    "revision": "941c1375fe21b97c8796",
     "url": "https://dendukuriravikiran.github.io/CMPE_285_frontend/static/css/main.7bedab26.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "https://dendukuriravikiran.github.io/CMPE_285_frontend/static/js/2.061c2734.chunk.js"
   },
   {
-    "revision": "bde8accedde27f17bbdf",
-    "url": "https://dendukuriravikiran.github.io/CMPE_285_frontend/static/js/main.6b2ec00c.chunk.js"
+    "revision": "941c1375fe21b97c8796",
+    "url": "https://dendukuriravikiran.github.io/CMPE_285_frontend/static/js/main.97e601a4.chunk.js"
   },
   {
     "revision": "f26c1ba649559fc1caad",
